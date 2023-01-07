efe24ffae34aaf56f3540d5f558ebfb533268cd8
HerrDima <dzmitry.turautsou@gmail.com>
2023-01-07 23:45:58 +0100

    test static

--- library-component/library-component.js
+++
@@ -1,7 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-import libraryStyle from './library.scss';
-export function LibraryComponent(props) {
-    const { children, textContent } = props;
-    return (_jsxs("div", { className: libraryStyle.test_library, children: [_jsx("h3", { className: libraryStyle.test_library__header, children: textContent }), _jsx("div", { children: children })] }));
-}
-//# sourceMappingURL=library-component.js.map
--- library.js
+++
@@ -1,2 +0,0 @@
-export { LibraryComponent as Library } from './library-component/library-component';
-//# sourceMappingURL=library.js.map
